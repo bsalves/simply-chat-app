fd0d81453f87ba18dd43c4bc61b7a9b0be404841
Bruno Cavaletti Alves <bcalves@apoioprodesp.gov.sp.br>
2024-05-15 15:27:50 -0300

    redis

--- config/initializers/redis.rb
+++
@@ -1,7 +0,0 @@
-require 'redis'
-
-p 'configure REDIS'
-
-redis.url = 'redis://default:gVFaW09h2DkQLG1NjVg5FZNnanF5WkDH@redis-16110.c90.us-east-1-3.ec2.redns.redis-cloud.com:16110'
-
-$redis = Redis.new(url: redis_url)
