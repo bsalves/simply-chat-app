2dbe17efd0cd469e6ada328410fabaec6cbb8651
Bruno Cavaletti Alves <bcalves@apoioprodesp.gov.sp.br>
2024-05-15 15:07:10 -0300

    put

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,9 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  config.cache_store = :redis_cache_store, { url: "redis://default:gVFaW09h2DkQLG1NjVg5FZNnanF5WkDH@redis-16110.c90.us-east-1-3.ec2.redns.redis-cloud.com:16110" }
+  p "production.rb"
+    
+  # config.cache_store = :redis_cache_store, { url: "redis://default:gVFaW09h2DkQLG1NjVg5FZNnanF5WkDH@redis-16110.c90.us-east-1-3.ec2.redns.redis-cloud.com:16110" }
 
   config.cache_store = :redis_store, 'redis://default:gVFaW09h2DkQLG1NjVg5FZNnanF5WkDH@redis-16110.c90.us-east-1-3.ec2.redns.redis-cloud.com:16110', {
     expires_in: 90.minutes
