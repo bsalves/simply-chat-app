3493966b2271ac7e29efd21cf0ea7d13fb158d3d
Bruno Cavaletti Alves <bcalves@apoioprodesp.gov.sp.br>
2024-05-15 15:14:28 -0300

    add redis init

---
+++ config/initializers/redis.rb
@@ -0,0 +1,6 @@
+ # 'redis://default:gVFaW09h2DkQLG1NjVg5FZNnanF5WkDH@redis-16110.c90.us-east-1-3.ec2.redns.redis-cloud.com:16110'
+ 
+ redis_host = 'redis-16110.c90.us-east-1-3.ec2.redns.redis-cloud.com'
+ redis_port = 16110
+
+ REDIS = Redis.new(host: redis_host, port: redis_port)
