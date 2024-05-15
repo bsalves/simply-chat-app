611d97fb6239e3197f34877d414cee626ccc47dd
Bruno Cavaletti Alves <bcalves@apoioprodesp.gov.sp.br>
2024-05-15 15:24:28 -0300

    redis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -4,4 +4,4 @@ p 'configure REDIS'
 
 redis.url = 'redis://default:gVFaW09h2DkQLG1NjVg5FZNnanF5WkDH@redis-16110.c90.us-east-1-3.ec2.redns.redis-cloud.com:16110'
 
-$redis = Redis.new(:url => redis.url)
+$redis = Redis.new(url: redis_url)
